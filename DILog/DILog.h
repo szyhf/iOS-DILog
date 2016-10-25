cd1d35bc29f64d17928cf427e6abcb855536f57c
Back Yu <szyhf@163.com>
2016-10-25 13:10:54 +0800

    init project

--- DILog/DILog.h
+++ DILog/DILog.h
@@ -16,4 +16,69 @@ FOUNDATION_EXPORT const unsigned char DILogVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DILog/PublicHeader.h>
 
+#ifndef DILog_h
+#define DILog_h
 
+#include "XCodeColors.h"
+
+#define nsFileString [[NSString stringWithUTF8String:__FILE__] substringFromIndex:1+[[NSString stringWithUTF8String:__FILE__] rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"/"] options:NSBackwardsSearch].location]
+#define DILogString(logType,format,...) [NSString stringWithFormat:@"[%@ %d]\n%@ %@",nsStringOfYellow(nsFileString),__LINE__,logType,nsStringOfGreen(([NSString stringWithFormat:format, ##__VA_ARGS__]))]
+
+//日志分级制度
+//#define DEBUG//XCODE默认有Debug定义，即无论是否定义DEBUG，都会输出Debug
+#define DI_INFO nsStringOfLight(@"[I]")
+#define DI_WARN nsStringOfOrange(@"[W]")
+#define DI_DEBUG nsStringOfCyan(@"[D]")
+#define DI_ERROR nsStringOfMagent(@"[E]")
+#define DI_FATAL nsStringOfRed(@"[F]")
+//#define DI_NOTICE "[N]"
+
+#ifdef DI_DEBUG //DEBUG Level指出细粒度信息事件对调试应用程序是非常有帮助的。
+#define DebugLog(format, ...) NSLog(@"%@\n\n",DILogString(DI_DEBUG,format,##__VA_ARGS__))
+#define DebugLogWhile(assert,format,...) if(assert)NSLog(@"%@",DILogString(DI_DEBUG,format,##__VA_ARGS__))
+#else
+#define DebugLog(format, ...)
+#define DebugLogWhile(assert,format,...)
+#endif
+
+#ifdef DI_INFO //INFO level表明 消息在粗粒度级别上突出强调应用程序的运行过程。
+#define InfoLog(format, ...) NSLog(@"%@",DILogString(DI_INFO,format,##__VA_ARGS__))
+#define InfoLogWhile(assert,format,...) if(assert)NSLog(@"%@",DILogString(DI_INFO,format,##__VA_ARGS__))
+#else
+#define InfoLog(format, ...)
+#define InfoLogWhile(assert,format,...)
+#endif
+
+#ifdef DI_WARN //WARN level表明会出现潜在错误的情形。
+#define WarnLog(format, ...) NSLog(@"%@",DILogString(DI_WARN,format,##__VA_ARGS__))
+#define WarnLogWhile(assert,format,...) if(assert)NSLog(@"%@",DILogString(DI_WARN,format,##__VA_ARGS__))
+#else
+#define WarnLog(format,...)
+#define WarnLogWhile(assert,format,...)
+#endif
+
+#ifdef DI_ERROR //ERROR level指出虽然发生错误事件，但仍然不影响系统的继续运行。
+#define ErrorLog(format, ...) NSLog(@"%@",DILogString(DI_ERROR,format,##__VA_ARGS__))
+#define ErrorLogWhile(assert,format,...) if(assert)NSLog(@"%@",DILogString(DI_ERROR,format,##__VA_ARGS__))
+#else
+#define ErrorLog(format, ...)
+#define ErrorLogWhile(assert,format,...)
+#endif
+
+#ifdef DI_FATAL //FATAL level指出每个严重的错误事件将会导致应用程序的退出。
+#define FatalLog(format, ...) NSLog(@"%@",DILogString(DI_FATAL,format,##__VA_ARGS__))
+#define FatalLogWhile(assert,format,...) if(assert)NSLog(@"%@",DILogString(DI_FATAL,format,##__VA_ARGS__))
+#else
+#define FatalLog(format, ...)
+#define FatalLogWhile(assert,format,...)
+#endif
+
+#ifdef DI_NOTICE //NOTICE level基础消息。
+#define NoticeLog(format, ...) NSLog(@"%@",DILogString(DI_NOTICE,format,##__VA_ARGS__))
+#define NoticeLogWhile(assert,format,...) if(assert)NSLog(@"%@",DILogString(DI_NOTICE,format,##__VA_ARGS__))
+#else
+#define NoticeLog(format, ...)
+#define NoticeLogWhile(assert,format,...)
+#endif
+
+#endif /* DILog_h */
--- DILog/XCodeColors.h
+++ DILog/XCodeColors.h
@@ -8,6 +8,63 @@
 
 #ifndef XCodeColors_h
 #define XCodeColors_h
+// <COPY ME> 基于XCodeColors的彩色输出=。=
 
+#define XCODE_COLORS_ESCAPE @"\033["
+
+#define XCODE_COLORS_RESET_FG  XCODE_COLORS_ESCAPE @"fg;" // Clear any foreground color
+#define XCODE_COLORS_RESET_BG  XCODE_COLORS_ESCAPE @"bg;" // Clear any background color
+#define XCODE_COLORS_RESET     XCODE_COLORS_ESCAPE @";"   // Clear any foreground or background color
+#define light_xcode_color
+// </COPY ME>
+#ifdef light_xcode_color
+
+#define XCODE_COLOR_Black @"0,0,0"
+#define XCODE_COLOR_Dark @"84,84,84"
+#define XCODE_COLOR_Light @"168,168,168"
+#define XCODE_COLOR_White @"220,220,220"
+#define XCODE_COLOR_Gray @"128,128,128"
+#define XCODE_COLOR_Red @"220,0,0"
+#define XCODE_COLOR_Green @"0,220,0"
+#define XCODE_COLOR_Blue @"0,0,220"
+#define XCODE_COLOR_Cyan @"0,220,220"
+#define XCODE_COLOR_Yellow @"220,220,0"
+#define XCODE_COLOR_Magent @"220,0,220"
+#define XCODE_COLOR_Orange @"220,128,0"
+#define XCODE_COLOR_Purple @"128,0,128"
+#define XCODE_COLOR_Brown @"153,102,51"
+
+#else
+
+#define XCODE_COLOR_Black @"0,0,0"
+#define XCODE_COLOR_Dark @"84,84,84"
+#define XCODE_COLOR_Light @"168,168,168"
+#define XCODE_COLOR_White @"255,255,255"
+#define XCODE_COLOR_Gray @"128,128,128"
+#define XCODE_COLOR_Red @"255,0,0"
+#define XCODE_COLOR_Green @"0,255,255"
+#define XCODE_COLOR_Blue @"0,0,255"
+#define XCODE_COLOR_Cyan @"0,255,255"
+#define XCODE_COLOR_Yellow @"255,255,0"
+#define XCODE_COLOR_Magent @"255,0,255"
+#define XCODE_COLOR_Orange @"255,128,0"
+#define XCODE_COLOR_Purple @"128,0,128"
+#define XCODE_COLOR_Brown @"153,102,51"
+
+#endif /* XCODE_COLOR */
+#define nsStringOfBlack(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Black,str]
+#define  nsStringOfDark(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Dark , str]
+#define  nsStringOfLight(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Light , str]
+#define  nsStringOfWhite(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_White , str]
+#define  nsStringOfGray(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Gray , str]
+#define  nsStringOfRed(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Red , str]
+#define  nsStringOfGreen(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Green , str]
+#define  nsStringOfBlue(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Blue , str]
+#define  nsStringOfCyan(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Cyan , str]
+#define  nsStringOfYellow(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Yellow , str]
+#define  nsStringOfMagent(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Magent , str]
+#define  nsStringOfOrange(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Orange , str]
+#define  nsStringOfPurple(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Purple , str]
+#define  nsStringOfBrown(str) [NSString stringWithFormat:@"\033[fg%@;%@\033[;",XCODE_COLOR_Brown , str]
 
 #endif /* XCodeColors_h */
