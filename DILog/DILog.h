b69b42029b507a64c6ab5836af294db351062c24
Back Yu <szyhf@163.com>
2016-10-25 12:47:55 +0800

    Initial Commit

---
+++ DILog/DILog.h
@@ -0,0 +1,19 @@
+//
+//  DILog.h
+//  DILog
+//
+//  Created by Back on 2016/10/25.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DILog.
+FOUNDATION_EXPORT double DILogVersionNumber;
+
+//! Project version string for DILog.
+FOUNDATION_EXPORT const unsigned char DILogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DILog/PublicHeader.h>
+
+
